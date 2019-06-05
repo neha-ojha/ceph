35654dda5299923a69b3a88970fd50ad3d3d4b51
Neha Ojha <nojha@redhat.com>
2019-06-05 12:23:04 -0700

    qa/tasks/cbt.py: test 40120
    Signed-off-by: Neha Ojha <nojha@redhat.com>

--- qa/tasks/cbt.py
+++ qa/tasks/cbt.py
@@ -173,8 +173,10 @@ class CBT(Task):
 
     def checkout_cbt(self):
         testdir = misc.get_testdir(self.ctx)
-        repo = self.config.get('repo', 'https://github.com/ceph/cbt.git')
-        branch = self.config.get('branch', 'master')
+        repo = self.config.get('repo', 'https://github.com/neha-ojha/cbt.git')
+        #repo = self.config.get('repo', 'https://github.com/ceph/cbt.git')
+        branch = self.config.get('branch', 'wip-40120')
+        #branch = self.config.get('branch', 'master')
         branch = self.config.get('force-branch', branch)
         sha1 = self.config.get('sha1')
         self.first_mon.run(
