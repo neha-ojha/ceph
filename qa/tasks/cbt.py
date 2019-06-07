32c5a037e560fcfbbc54f0a550fa55629fb23957
Neha Ojha <nojha@redhat.com>
2019-06-07 12:08:42 -0700

    qa: adapt cbt task and perf-basic to client_endpoints
    Signed-off-by: Neha Ojha <nojha@redhat.com>

--- qa/tasks/cbt.py
+++ qa/tasks/cbt.py
@@ -46,9 +46,11 @@ class CBT(Task):
 
         benchmark_config = self.config.get('benchmarks')
         benchmark_type = benchmark_config.keys()[0]
-        if benchmark_type == 'librbdfio':
+        #if benchmark_type == 'librbdfio':
+        if benchmark_type == 'fio':
           testdir = misc.get_testdir(self.ctx)
-          benchmark_config['librbdfio']['cmd_path'] = os.path.join(testdir, 'fio/fio')
+          #benchmark_config['librbdfio']['cmd_path'] = os.path.join(testdir, 'fio/fio')
+          benchmark_config['fio']['cmd_path'] = os.path.join(testdir, 'fio/fio')
         if benchmark_type == 'cosbench':
             # create cosbench_dir and cosbench_xml_dir
             testdir = misc.get_testdir(self.ctx)
@@ -61,11 +63,13 @@ class CBT(Task):
             remotes_and_roles = self.ctx.cluster.remotes.items()
             ips = [host for (host, port) in
                    (remote.ssh.get_transport().getpeername() for (remote, role_list) in remotes_and_roles)]
-            benchmark_config['cosbench']['auth'] = "username=cosbench:operator;password=intel2012;url=http://%s:80/auth/v1.0;retry=9" %(ips[0])
+            benchmark_config['cosbench']['auth'] = "username=cosbench:operator;password=intel2012;url=http://%s:80/auth/v1.0;retry=9" %(ips[0]) 
+        client_endpoints_config = self.config.get('client_endpoints', None)
 
         return dict(
             cluster=cluster_config,
             benchmarks=benchmark_config,
+            client_endpoints = client_endpoints_config,
             )
 
     def install_dependencies(self):
@@ -82,7 +86,8 @@ class CBT(Task):
         benchmark_type = self.cbt_config.get('benchmarks').keys()[0]
         self.log.info('benchmark: %s', benchmark_type)
 
-        if benchmark_type == 'librbdfio':
+        #if benchmark_type == 'librbdfio':
+        if benchmark_type == 'fio':
             # install fio
             testdir = misc.get_testdir(self.ctx)
             self.first_mon.run(
@@ -228,7 +233,8 @@ class CBT(Task):
             ]
         )
         benchmark_type = self.cbt_config.get('benchmarks').keys()[0]
-        if benchmark_type == 'librbdfio':
+        #if benchmark_type == 'librbdfio':
+        if benchmark_type == 'fio':
             self.first_mon.run(
                 args=[
                     'rm', '--one-file-system', '-rf', '--',
